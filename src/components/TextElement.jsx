d49984d40745b5d9402f4a5edfa5f7e3f774f387
Pavel Zapletal <wardreth@gmail.com>
2021-03-09 20:13:59 +0100

    latest

--- src/components/TextElement.jsx
+++ src/components/TextElement.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
 	buyCake,
@@ -8,11 +9,18 @@ import {
 	alterUserData,
 } from "../redux";
 const TextElement = () => {
+	const [persons, setPersons] = useState([]);
 	const numOfCakes = useSelector(state => state.cake.numOfCakes);
 	const numOfIcecreams = useSelector(state => state.icecream.numOfIcecreams);
+	// const getData = useSelector(async state => await state.user)
+	// 	.then(resp => setPersons(resp.users))
+	// 	.catch(err => err.message);
+	const getData = useSelector(state => state.user.users);
 
 	const dispatch = useDispatch();
-
+	const fetchHandler = () => {
+		dispatch(fetchUsers());
+	};
 	return (
 		<div>
 			<p>Number of cakes - {numOfCakes}</p>
@@ -23,10 +31,17 @@ const TextElement = () => {
 			<button onClick={() => dispatch(buyMultipleIcecreams(5))}>
 				Buy 5 icecreams
 			</button>
-			<button onClick={() => dispatch(fetchUsers())}> fetchni users</button>
+
+			<button onClick={fetchHandler}> fetchni users</button>
+			{/* <button onClick={() => dispatch(fetchHandler)}> fetchni users</button> //this worked lol */}
+			{/* <button onClick={() => dispatch(fetchUsers())}> fetchni users</button> */}
 			<button onClick={() => dispatch(alterUserData(["Pavel", "Bara"]))}>
 				Alter data
 			</button>
+
+			{getData.map((el, index) => (
+				<div key={index}>{el.name}</div>
+			))}
 		</div>
 	);
 };
